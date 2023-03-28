7bf8f73dff2b65450e7f41c806568ed0959d2f07
Leon Zuev <loonatune@gmail.com>
2023-03-28 22:56:03 +0300

    Add commands

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 package src;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
   enum Command {
@@ -10,5 +13,15 @@ public class Main {
     SORT_LIST, // сортировать
     EXPORT_LIST, // экспорт
     EXIT, // выход из программы
+  }
+  private static final Map<Command, String> commands = new HashMap<>();
+  static {
+    commands.put(Command.HELP, "Список команд");
+    commands.put(Command.NEW_LIST, "Новая запись");
+    commands.put(Command.CHECK_LIST, "Проверить запись");
+    commands.put(Command.CORRECT_LIST, "Править запись");
+    commands.put(Command.SORT_LIST, "Сортировать записи");
+    commands.put(Command.EXPORT_LIST, "Экспортировать запись");
+    commands.put(Command.EXIT, "ВЫХОД");
   }
 }
