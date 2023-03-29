c1199bc10bb2002ca8b5705643c438f859464d47
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-29 13:51:46 +0200

    refactor correctTask()

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class ToDoList {
 
-  private final Map<Integer,Test> current = new HashMap<>();
+  private final Map<Integer, Test> current = new HashMap<>();
 
   public void newTask(Test task) { //добавить задачу в текущий список дел
     current.put(task.getId(), task);
@@ -28,12 +28,17 @@ public class ToDoList {
     BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
     System.out.print("Please, enter the task number:");
-    int id = Integer.parseInt(read.readLine());
+    int id = Integer.parseInt(read.readLine()); //todo словить ошибки ввода
 
     System.out.print("What do you want to change?");
     String forChange = read.readLine(); //todo сделать енам для выбора и while true
 
+    switch (forChange) { //todo добавить все варианты изменения
+      case ("title"):
+        System.out.print("Please, enter new title:");
+        String newTitle = read.readLine();
+        current.get(id).setTaskName(newTitle);
+
+    }
   }
-
-
 }
