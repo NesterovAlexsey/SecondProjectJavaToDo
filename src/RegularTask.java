b1bb4b5a318f4ff3c7a6cb3efd31f7dd4d7f4c37
Ion Cicala <icicala8@gmail.com>
2023-03-29 18:55:19 +0200

    comm task

--- src/RegularTask.java
+++ src/RegularTask.java
@@ -2,12 +2,14 @@ package src;
 
 import java.lang.reflect.Type;
 
-public class RegularTask implements TypaskType {
+public class RegularTask  {
   private final int taskId;
   private final int hours;
   private final int minutes;
-  private final String taskTitle;
-  private final Type taskType;
+  private String taskTitle;
+  private Type taskType;
+  //private Type taskType;
+
   public RegularTask(int taskId, int hours, int minutes, String taskTitle, Type taskType) {
     this.taskId = taskId;
     this.hours = hours;
@@ -15,24 +17,45 @@ public class RegularTask implements TypaskType {
     this.taskTitle = taskTitle;
     this.taskType = taskType;
   }
+
   public int getTaskId() {
     return taskId;
   }
+
   public int getHours() {
     return hours;
   }
+
   public int getMinutes() {
     return minutes;
   }
+
   public String getTaskTitle() {
     return taskTitle;
   }
+
   public Type getTaskType() {
     return taskType;
   }
+
   @Override
   public String toString() {
     return "Task " + taskId + " - " + taskTitle + " (" + hours + "h " + minutes + "m) " +
             "[" + taskType + "]";
+  }
+
+  public void setTaskHours
+          (int taskHours) {
+  }
+
+  public void setTaskMinutes(int taskMinutes) {
+  }
+
+  public void setTaskTitle(String taskTitle) {
+    this.taskTitle = taskTitle;
+  }
+
+  public void setTaskType(Type taskType) {
+    this.taskType = taskType;
   }
 }
