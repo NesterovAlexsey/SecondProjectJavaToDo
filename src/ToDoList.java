d4cdd4ec906b566aae5818b456c9f8d8b5a8cc4c
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-29 20:49:36 +0200

    refactor correctTask()

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -4,21 +4,46 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ToDoList {
 
+  // Блок методов к Енум
+  enum Correction {
+    CHANGE_TITLE, //коррекция название
+    CHANGE_HOURS, // коррекция часов
+    CHANGE_MINUTES, // коррекция минут
+    DELETE_TASK, // удаление
+    FINISH, // закончить коррекцию
+  }
 
+  private static final List<Correction> corrections = new ArrayList<>();
+
+  static {
+    corrections.add(Correction.CHANGE_TITLE);
+    corrections.add(Correction.CHANGE_HOURS);
+    corrections.add(Correction.CHANGE_MINUTES);
+    corrections.add(Correction.DELETE_TASK);
+    corrections.add(Correction.FINISH);
+  }
+
+  private void printMenu() {
+    System.out.println();
+    System.out.println("Possible updates: ");
+    for (Correction correction : corrections) {
+      System.out.println(correction);
+    }
+  }
 
   private final Map<Integer, RegularTask> current = new HashMap<>();
 
+  //метод, вызываемыйе в мейн
   public void newTask(RegularTask task) { //добавить задачу в текущий список дел
     current.put(task.getTaskId(), task);
+    checkList();
   }
 
+  //метод, вызываемыйе в мейн
   public void checkList() { //показать экран
     List<Map.Entry<Integer, RegularTask>> listOfTaskWithId = new ArrayList<>();
 
@@ -35,27 +60,75 @@ public class ToDoList {
     }
   }
 
+  //метод, вызываемыйе в мейн
   public void correctTask() throws IOException { //корректировать задачу
     BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
     System.out.print("Please, enter the task number:");
     int id = Integer.parseInt(read.readLine()); //todo словить ошибки ввода
 
-    System.out.print("What do you want to change?");
-    String forChange = read.readLine(); //todo сделать енам для выбора и while true
+    Correction correction = readCorrection();
 
-    switch (forChange) { //todo добавить все варианты изменения
-      case ("title"):
-        System.out.print("Please, enter new title:");
-        String newTitle = read.readLine();
-        current.get(id).setTaskTitle(newTitle);
+    switch (correction) {
+      case CHANGE_TITLE -> changeTitleInTask(id, read);
+      case CHANGE_HOURS -> changeHoursInTask(id, read);
+      case CHANGE_MINUTES -> changeMinutesInTask(id, read);
+      case DELETE_TASK -> deleteTask(id);
+      case FINISH -> checkList(); //todo усовершенствовать вариант финиш
     }
   }
 
+  //todo - словить ошибки у методов коррекции
+  private void changeTitleInTask(Integer id, BufferedReader read) throws IOException {
+    System.out.print("Please, enter new title:");
+    String newTitle = read.readLine();
+    current.get(id).setTaskTitle(newTitle);
+  }
+
+  private void changeHoursInTask(Integer id, BufferedReader read) throws IOException {
+    System.out.print("Please, enter new hours:");
+    int temp = Integer.parseInt(read.readLine());
+    current.get(id).setTaskHours(temp);
+  }
+
+  private void changeMinutesInTask(Integer id, BufferedReader read) throws IOException {
+    System.out.print("Please, enter new minutes:");
+    int temp = Integer.parseInt(read.readLine());
+    current.get(id).setTaskMinutes(temp);
+  }
+
+  private void deleteTask(int id) {
+    current.remove(id);
+  }
+
+  private Correction readCorrection() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    printMenu();
+    System.out.println("What do you want to change?");
+    String correction = br.readLine().toUpperCase();
+
+    Correction result = null;
+    while (result == null) {
+      try {
+        result = Correction.valueOf(correction);
+      } catch (IllegalArgumentException e) {
+        System.out.println("Некорректная команда: " + correction);
+        System.out.println("Введите корректную команду: ");
+        correction = br.readLine().toUpperCase();
+      }
+    }
+
+    System.out.println();
+    return result;
+  }
+
+  //метод, вызываемыйе в мейн
   public void sortTasks() { //сортировать задачи
     //Todo создать отдельный Comparator, здесь вызвать
   }
 
+  //метод, вызываемыйе в мейн
   public void exportTaskList() throws IOException { // экспортировать задачи в файл
     //todo записать дату в файл, записать мапу в нужном формате, закрыть файл
     FileWriter ToDoList = new FileWriter("res/ToDoList.txt", true);
