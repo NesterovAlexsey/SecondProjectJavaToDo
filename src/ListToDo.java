42dd30ea222ca63b9a0343a492b0cc6f1aa9da21
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-30 07:06:35 +0200

    start to update ListToDo

--- src/ListToDo.java
+++ src/ListToDo.java
@@ -1,4 +1,16 @@
 package src;
 
+import java.util.Map;
+
 public class ListToDo {
+
+  private Map.Entry<Integer, RegularTask> idTasks;
+  public ListToDo(Map.Entry<Integer, RegularTask> idTasks){
+    this.idTasks = idTasks;
+  }
+
+  public void printListToDo(){
+
+  }
+
 }
