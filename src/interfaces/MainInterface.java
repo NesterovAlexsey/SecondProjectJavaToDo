7dcf83ec0a215a2c3e01539dcba529ac3329594e
LeonZuev <loonatune@gmail.com>
2023-04-15 06:17:09 +0300

    Fix mistakes in MainInterface

--- src/interfaces/MainInterface.java
+++ src/interfaces/MainInterface.java
@@ -1,16 +1,9 @@
 package src.interfaces;
-
-
-
 import src.toDoList.RegularTask;
 import src.toDoList.ToDoList;
-import src.toDoList.Main;
-import src.toDoList.*;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class MainInterface {
@@ -56,40 +49,31 @@ public class MainInterface {
     panel.add(button1);
 
     // Add ActionListener to HELP button
-    button1.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(frame, "This is the help message!");
-      }
-    });
+    button1.addActionListener(e -> JOptionPane.showMessageDialog(frame, "This is the help message!"));
 
 
     JButton button2 = new JButton("New task");
     panel.add(button2);
 
-    button2.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        JTextField title = new JTextField();
-        JTextField hours = new JTextField();
-        JTextField minutes = new JTextField();
-        Object[] fields = {"Title: ", title, "Hours", hours, "Minutes", minutes};
-        int option = JOptionPane.showConfirmDialog(frame, fields, "New Task",
-                JOptionPane.OK_CANCEL_OPTION);
-        if (option == JOptionPane.OK_OPTION) {
-          try {
-            int taskHours = Integer.parseInt(hours.getText());
-            int taskMinutes = Integer.parseInt(minutes.getText());
-            ToDoList currentTodoList = new ToDoList();
-            Object ToDoList = null;
-            int taskId = currentTodoList.getNextTaskId();
-            RegularTask newTask = new RegularTask(taskId, taskHours, taskMinutes,
-                    title.getText());
-            ToDoList currentToDoList = new ToDoList();
-            currentToDoList.newTask(newTask);
-          } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(frame, "Invalid input. Please enter numeric values for hours and minutes.");
-          }
+    button2.addActionListener(e -> {
+      JTextField title = new JTextField();
+      JTextField hours = new JTextField();
+      JTextField minutes = new JTextField();
+      Object[] fields = {"Title: ", title, "Hours", hours, "Minutes", minutes};
+      int option = JOptionPane.showConfirmDialog(frame, fields, "New Task",
+              JOptionPane.OK_CANCEL_OPTION);
+      if (option == JOptionPane.OK_OPTION) {
+        try {
+          int taskHours = Integer.parseInt(hours.getText());
+          int taskMinutes = Integer.parseInt(minutes.getText());
+          ToDoList currentTodoList = new ToDoList();
+          int taskId = currentTodoList.getNextTaskId();
+          RegularTask newTask = new RegularTask(taskId, taskHours, taskMinutes,
+                  title.getText());
+          ToDoList currentToDoList = new ToDoList();
+          currentToDoList.newTask(newTask);
+        } catch (NumberFormatException ex) {
+          JOptionPane.showMessageDialog(frame, "Invalid input. Please enter numeric values for hours and minutes.");
         }
       }
     });
@@ -97,39 +81,33 @@ public class MainInterface {
     JButton button3 = new JButton("Check task");
     panel.add(button3);
 
-    button3.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setEditable(false);
-        for (RegularTask task : todoList.getCurrentTasks()) {
-          textArea.append(task.toString() + "\n");
-        }
-        JOptionPane.showMessageDialog(frame, scrollPane, "Current Tasks", JOptionPane.PLAIN_MESSAGE);
+    button3.addActionListener(e -> {
+      JTextArea textArea = new JTextArea();
+      JScrollPane scrollPane = new JScrollPane(textArea);
+      textArea.setEditable(false);
+      for (RegularTask task : todoList.getCurrentTasks()) {
+        textArea.append(task.toString() + "\n");
       }
+      JOptionPane.showMessageDialog(frame, scrollPane, "Current Tasks", JOptionPane.PLAIN_MESSAGE);
     });
 
     JButton button4 = new JButton("Correct task");
     panel.add(button4);
 
-    button4.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        JTextField taskIdField = new JTextField();
-        Object[] fields = {"Task ID: ", taskIdField};
-        int option = JOptionPane.showConfirmDialog(frame, fields, "Correct Task", JOptionPane.OK_CANCEL_OPTION);
-        if (option == JOptionPane.OK_OPTION) {
-          try {
-            int taskId = Integer.parseInt(taskIdField.getText());
-            todoList.correctTask(taskId);
-          } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(frame, "Invalid input. Please enter a numeric value for the task ID.");
-          } catch (IllegalArgumentException ex) {
-            JOptionPane.showMessageDialog(frame, ex.getMessage());
-          } catch (Exception ex) {
-            JOptionPane.showMessageDialog(frame, "An error occurred: " + ex.getMessage());
-          }
+    button4.addActionListener(e -> {
+      JTextField taskIdField = new JTextField();
+      Object[] fields = {"Task ID: ", taskIdField};
+      int option = JOptionPane.showConfirmDialog(frame, fields, "Correct Task", JOptionPane.OK_CANCEL_OPTION);
+      if (option == JOptionPane.OK_OPTION) {
+        try {
+          int taskId = Integer.parseInt(taskIdField.getText());
+          todoList.correctTask(taskId);
+        } catch (NumberFormatException ex) {
+          JOptionPane.showMessageDialog(frame, "Invalid input. Please enter a numeric value for the task ID.");
+        } catch (IllegalArgumentException ex) {
+          JOptionPane.showMessageDialog(frame, ex.getMessage());
+        } catch (Exception ex) {
+          JOptionPane.showMessageDialog(frame, "An error occurred: " + ex.getMessage());
         }
       }
     });
@@ -137,49 +115,42 @@ public class MainInterface {
     JButton button5 = new JButton("Sort task");
     panel.add(button5);
 
-    button5.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        try {
-          ToDoList currentToDoList;
-          currentToDoList = null;
-          currentToDoList.sortTasks();
-        } catch (IOException ioException) {
-          ioException.printStackTrace();
-        }
-      }
-    });
+    button5.addActionListener(MainInterface::actionPerformed);
 
     JButton button6 = new JButton("Export task");
     panel.add(button6);
 
-    button6.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        try {
-          ToDoList currentToDoList = null;
-          currentToDoList.exportTaskList();
-          JOptionPane.showMessageDialog(frame, "Task list exported successfully!");
-        } catch (IOException ex) {
-          JOptionPane.showMessageDialog(frame, "Error exporting task list: " + ex.getMessage());
-        }
+    button6.addActionListener(e -> {
+      try {
+        ToDoList currentToDoList = null;
+        currentToDoList.exportTaskList();
+        JOptionPane.showMessageDialog(frame, "Task list exported successfully!");
+      } catch (IOException ex) {
+        JOptionPane.showMessageDialog(frame, "Error exporting task list: " + ex.getMessage());
       }
     });
 
     JButton button7 = new JButton("EXIT");
     panel.add(button7);
 
-    button7.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        int option = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
-        if (option == JOptionPane.YES_OPTION) {
-          System.exit(0);
-        }
+    button7.addActionListener(e -> {
+      int option = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
+      if (option == JOptionPane.YES_OPTION) {
+        System.exit(0);
       }
     });
 
     frame.add(panel);
     frame.setVisible(true);
+  }
+
+  private static void actionPerformed(ActionEvent e) {
+    try {
+      ToDoList currentToDoList;
+      currentToDoList = null;
+      currentToDoList.sortTasks();
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
+    }
   }
 }
